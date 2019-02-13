dc26b846bc5785fa456c291d8ec37a24650b0c6c
James Yang <jamesyang2333@gmail.com>
2019-02-13 13:32:22 +0800

    changed the scaling filter for semantic labels

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -61,7 +61,7 @@ class CreateDataset(data.Dataset):
             lab_target = Image.open(lab_target_path)
             #lab_source = Image.open(lab_source_path).convert('RGB')
             lab_target = Image.open(lab_target_path).convert('RGB')
-            lab_source = lab_source.resize([self.opt.loadSize[0], self.opt.loadSize[1]], Image.BICUBIC)
+            lab_source = lab_source.resize([self.opt.loadSize[0], self.opt.loadSize[1]], Image.NEAREST)
             lab_source = np.array(lab_source)
             if lab_source.max() > 12:
                 print("error!!!!!")
