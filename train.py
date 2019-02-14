f1baa520016e1bbe569ef9876a6afd874aa3ef54
James Yang <jamesyang2333@gmail.com>
2019-02-14 13:54:40 +0800

    .

--- train.py
+++ train.py
@@ -26,17 +26,17 @@ for epoch in range(opt.epoch_count, opt.niter+opt.niter_decay+1):
         model.set_input(data)
         model.optimize_parameters(i)
 
-        # if total_steps % opt.display_freq == 0:
-        #     if epoch >= opt.transform_epoch:
-        #         model.validation_target()
-        #     visualizer.display_current_results(model.get_current_visuals(), epoch)
-        #
-        # if total_steps % opt.print_freq == 0:
-        #     errors = model.get_current_errors()
-        #     t = (time.time() - iter_start_time) / opt.batchSize
-        #     visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-        #     if opt.display_id > 0:
-        #         visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+        if total_steps % opt.display_freq == 0:
+            if epoch >= opt.transform_epoch:
+                model.validation_target()
+            # visualizer.display_current_results(model.get_current_visuals(), epoch)
+
+        if total_steps % opt.print_freq == 0:
+            errors = model.get_current_errors()
+            t = (time.time() - iter_start_time) / opt.batchSize
+            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            # if opt.display_id > 0:
+            #     visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' % (epoch, total_steps))
