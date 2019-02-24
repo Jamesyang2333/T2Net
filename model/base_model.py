fe91631094528f5ca17c4239b95f7fbdca7fc61e
James Yang <jamesyang2333@gmail.com>
2019-02-24 14:54:06 +0800

    .

--- model/base_model.py
+++ model/base_model.py
@@ -64,9 +64,9 @@ class BaseModel():
             if isinstance(name, str):
                 save_filename = '%s_net_%s.pth' % (which_epoch, name)
                 save_path = os.path.join(self.save_dir, save_filename)
-                print(save_filename)
-                print(save_path)
                 net = getattr(self, 'net_'+name)
+                print(net.state_dict().keys())
+                print(torch.load(save_path).state_dict().keys())
                 net.load_state_dict(torch.load(save_path))
                 if not self.isTrain:
                     net.eval()
